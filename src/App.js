4b4134d25a45f3956c2d802f98d178154eecb0bf
tiggrra <78557674+tiggrra@users.noreply.github.com>
2021-06-20 10:52:50 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo}></img>
+      </header>
+    </div>
+  );
+}
+
+export default App;
