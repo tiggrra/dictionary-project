7a1af273b30c53e3807f2f349392a20836ca4a67
tiggrra <78557674+tiggrra@users.noreply.github.com>
2021-06-20 11:39:36 +0300

    import components Footer, Dictionary

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+import React from "react";
 import logo from './logo.jpg';
+import Dictionary from "./Dictionary";
+import Footer from "./Footer";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} alt="logo"></img>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} alt="logo"></img>
+        </header>
+        <Dictionary />
+        <Footer />
+      </div>
     </div>
   );
 }
