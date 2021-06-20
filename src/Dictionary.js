64a8ca5b6a68402c3a7584d42dd16a8e285068bb
tiggrra <78557674+tiggrra@users.noreply.github.com>
2021-06-20 13:15:52 +0300

    added looping through results and multiple meaning display

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,21 +1,22 @@
 import axios from "axios";
 import React, { useState } from "react";
+import Results from "./Results";
 
 import "./Dictionary.css";
 
 export default function Dictionary() {
     let [keyword, setKeyword] = useState(null);
+    let [results, setResults] = useState(null);
 
     function search(event) {
         event.preventDefault();
-        alert(`Searching for ${keyword}`)
-
+        
         let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
         axios.get(apiUrl).then(showDefinition);
     }
 
     function showDefinition(response) {
-        console.log(response.data[0]);
+        setResults(response.data[0]);
     }
 
     function handleKeywordChange(event) {
@@ -27,5 +28,6 @@ export default function Dictionary() {
         <form onSubmit={search}>
             <input type="search" placeholder="Enter a word to search" autoFocus="on" onChange={handleKeywordChange}/>
         </form>
+        <Results results={results}/>
     </div>
 }
---
+++ src/Meaning.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default function Meaning(props) {
+    console.log(props.meaning);
+    return <div className="Meaning">
+        Meaning will be placed here
+    </div>
+}
