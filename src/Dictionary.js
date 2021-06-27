f3658904a6ac4c0d14f07eee0c929eed1ba161a4
tiggrra <78557674+tiggrra@users.noreply.github.com>
2021-06-27 12:05:05 +0200

    added Photos

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 
 import "./Dictionary.css";
 
@@ -8,10 +9,16 @@ export default function Dictionary(props) {
     let [keyword, setKeyword] = useState(props.defaultKeyword);
     let [results, setResults] = useState(null);
     let [loaded, setLoaded] = useState(false);
+    let [photos, setPhotos] = useState(null);
 
     function search() {
         let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
         axios.get(apiUrl).then(showDefinition);
+
+        let pexelsApiKey = "563492ad6f917000010000012ed3ab8d58a349c2bb986d961275f65b";
+        let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+        let headers = {"Authorization" : `Bearer ${pexelsApiKey}`}
+        axios.get(pexelsApiUrl, {headers: headers}).then(showPictures);
     }
     
     function handleSubmit(event) {
@@ -21,6 +28,10 @@ export default function Dictionary(props) {
 
     function showDefinition(response) {
         setResults(response.data[0]);
+    }
+
+    function showPictures(response) {
+        setPhotos(response.data.photos);
     }
 
     function handleKeywordChange(event) {
@@ -42,6 +53,7 @@ export default function Dictionary(props) {
                 </form>
             </section>
             <Results results={results} />
+            <Photos photos={photos} />
         </div>
     } else {
         load();
